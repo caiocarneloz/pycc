5cb9736b76615099d17b953cfdd38e29ca72a14a
caiocarneloz <caiocarneloz@gmail.com>
2019-07-11 22:07:19 -0300

    f_class

--- pcc.py
+++ pcc.py
@@ -10,98 +10,103 @@ from sklearn.metrics import precision_score
 
 class ParticleCompetitionAndCooperation():
 
-    def __init__(self, n_neighbours=1, pgrd=0.5, delta_v=0.35, max_iter=1000):
+    def __init__(self, n_neighbors=1, pgrd=0.5, delta_v=0.35, max_iter=1000, kernel='knn'):
 
-        self.n_neighbours = n_neighbours
+        self.n_neighbors = n_neighbors
         self.pgrd = pgrd
         self.delta_v = delta_v
         self.max_iter = max_iter
         self.accuracy_score = 0
-
+        self.storage = {}
+        self.graph = {}
+        self.c = 0
+        self.labels = []
+        self.data = None
+        
     def predict(self, data, labels):
 
         start = time.time()
+		
+        self.data = data
+        self.labels = labels
+        self.c = len(np.unique(self.labels))
 
-        storage = {}
+        self.storage['class_map'] = self.__genClassMap()
+        self.storage['particles'] = self.__genParticles()
+        self.storage['nodes'] = self.__genNodes()
+        self.storage['dist_table'] = self.__genDistTable()
 
-        c = len(np.unique(labels))
+        self.graph = self.__genGraph()
 
-        storage['class_map'] = self.__genClassMap(labels)
-        storage['particles'] = self.__genParticles(data, labels)
-        storage['nodes'] = self.__genNodes(data, labels, storage['particles'], c, storage['class_map'])
-        storage['dist_table'] = self.__genDistTable(data, storage['particles'])
-
-        graph = self.__genGraph(data)
-
-        self.__labelPropagation(graph, storage, labels, c)
+        self.__labelPropagation()
 
         end = time.time()
 
         print('finished with time: '+"{0:.0f}".format(end-start)+'s')
 
-        return list(storage['nodes'][:,0])
+        return list(self.storage['nodes'][:,0])
     
-    def __labelPropagation(self, graph, storage, labels, c):
+    def __labelPropagation(self):
 
         for it in range(0,self.max_iter):
 
-            for p_i in range(0,len(storage['particles'])):
+            for p_i in range(0,len(self.storage['particles'])):
                 if(np.random.random() < self.pgrd):
-                    next_node = self.__greedyWalk(storage, p_i, graph[storage['particles'][p_i,0]])
+                    next_node = self.__greedyWalk(p_i, self.graph[self.storage['particles'][p_i,0]])
                 else:
-                    next_node = self.__randomWalk(graph[storage['particles'][p_i,0]])
+                    next_node = self.__randomWalk(self.graph[self.storage['particles'][p_i,0]])
 
-                self.__update(storage, next_node, p_i, labels, c)
+                self.__update(next_node, p_i)
 
-        label_list = np.unique(labels[labels != -1])
+        label_list = np.unique(self.labels[self.labels != -1])
 
-        for n_i in range(0,len(storage['nodes'])):
-            if(storage['nodes'][n_i,0] == -1):
-                storage['nodes'][n_i,0] = label_list[np.argmax(storage['nodes'][n_i,1:])]
+        for n_i in range(0,len(self.storage['nodes'])):
+            if(self.storage['nodes'][n_i,0] == -1):
+                self.storage['nodes'][n_i,0] = label_list[np.argmax(self.storage['nodes'][n_i,1:])]
     
-    def __update(self, storage, n_i, p_i, labels, c):
+    def __update(self, n_i, p_i):
 
         current_domain = []
         new_domain = []
 
-        if(labels[n_i] == -1):
-            sub = (self.delta_v*storage['particles'][p_i,2])/(len(labels)-1)
+        if(self.labels[n_i] == -1):
+            sub = (self.delta_v*self.storage['particles'][p_i,2])/(len(self.labels)-1)
 
-            for l in np.unique(labels):
-                if(storage['particles'][p_i,3] != l and l != -1):
-                    current_domain.append(storage['nodes'][n_i,storage['class_map'][l]])
-                    storage['nodes'][n_i,storage['class_map'][l]] = max([0,storage['nodes'][n_i,storage['class_map'][l]]-sub])
-                    new_domain.append(storage['nodes'][n_i,storage['class_map'][l]])
+            for l in np.unique(self.labels):
+                if(self.storage['particles'][p_i,3] != l and l != -1):
+                    current_domain.append(self.storage['nodes'][n_i,self.storage['class_map'][l]])
+                    self.storage['nodes'][n_i,self.storage['class_map'][l]] = max([0,self.storage['nodes'][n_i,self.storage['class_map'][l]]-sub])
+                    new_domain.append(self.storage['nodes'][n_i,self.storage['class_map'][l]])
 
 
             difference = []
             for i in range(0,len(current_domain)):
                 difference.append(current_domain[i]-new_domain[i])
 
-            storage['nodes'][n_i,storage['class_map'][storage['particles'][p_i,3]]] += sum(difference)
+            self.storage['nodes'][n_i,self.storage['class_map'][self.storage['particles'][p_i,3]]] += sum(difference)
         else:
             new_domain.append(0)
 
-        storage['particles'][p_i,2] = storage['nodes'][n_i,storage['class_map'][storage['particles'][p_i,3]]]
+        self.storage['particles'][p_i,2] = self.storage['nodes'][n_i,self.storage['class_map'][self.storage['particles'][p_i,3]]]
 
-        current_node = storage['particles'][p_i,0]
-        if(storage['dist_table'][n_i,p_i] > (storage['dist_table'][current_node,p_i]+1)):
-            storage['dist_table'][n_i,p_i] = storage['dist_table'][current_node,p_i]+1
+        current_node = self.storage['particles'][p_i,0]
+        if(self.storage['dist_table'][n_i,p_i] > (self.storage['dist_table'][current_node,p_i]+1)):
+            self.storage['dist_table'][n_i,p_i] = self.storage['dist_table'][current_node,p_i]+1
 
-        if(storage['nodes'][n_i,storage['class_map'][storage['particles'][p_i,3]]] > max(new_domain)):#NEW OR CURRENT?
-            storage['particles'][p_i,0] = n_i
+        if(self.storage['nodes'][n_i,self.storage['class_map'][self.storage['particles'][p_i,3]]] > max(new_domain)):#NEW OR CURRENT?
+            self.storage['particles'][p_i,0] = n_i
 
-    def __greedyWalk(self, storage, p_i, neighbours):
+    def __greedyWalk(self, p_i, neighbors):
 
         prob_sum = 0
         slices = []
-        label = storage['particles'][p_i,3]
+        label = self.storage['particles'][p_i,3]
 
-        for n in neighbours:
-            prob_sum += storage['nodes'][n,storage['class_map'][label]]*(1/pow(1+storage['dist_table'][n,p_i],2))
+        for n in neighbors:
+            prob_sum += self.storage['nodes'][n,self.storage['class_map'][label]]*(1/pow(1+self.storage['dist_table'][n,p_i],2))
 
-        for n in neighbours:
-            slices.append((storage['nodes'][n,storage['class_map'][label]]*(1/pow(1+storage['dist_table'][n,p_i],2)))/prob_sum)
+        for n in neighbors:
+            slices.append((self.storage['nodes'][n,self.storage['class_map'][label]]*(1/pow(1+self.storage['dist_table'][n,p_i],2)))/prob_sum)
 
         choice = 0
         roullete_sum = 0
@@ -113,12 +118,12 @@ class ParticleCompetitionAndCooperation():
                 choice = i
                 break
 
-        return neighbours[choice]
+        return neighbors[choice]
     
     
-    def __randomWalk(self, neighbours):
+    def __randomWalk(self, neighbors):
 
-        return neighbours[np.random.choice(len(neighbours))]
+        return neighbors[np.random.choice(len(neighbors))]
     
     def accuracyScore(self, masked_labels, true_labels, predicted_labels):
     
@@ -151,69 +156,70 @@ class ParticleCompetitionAndCooperation():
     
         print('\n\nAccuracy: \n'+str(sum_pred) + '/' +str(sum_true) + ' - ' + "{0:.2f}".format(sum_pred/sum_true))
     
-    def __genClassMap(self, true_labels):
+    def __genClassMap(self):
     
         i = 1
         class_map = {}
-        for c in np.unique(true_labels):
+        for c in np.unique(self.labels):
             if(c != -1):
                 class_map[c] = i
                 i+=1
     
         return class_map
     
-    def __genParticles(self, data, labels):
+    def __genParticles(self):
     
         particles = []
     
-        for i in range(0,len(labels)):
-            if(labels[i] != -1):
-                particles.append([int(i),int(i),1,labels[i]])
+        for i in range(0,len(self.labels)):
+            if(self.labels[i] != -1):
+                particles.append([int(i),int(i),1,self.labels[i]])
     
         return np.array(particles, dtype='object')
     
-    def __genNodes(self, data, labels, particles, c, class_map):
+    def __genNodes(self):
     
-        nodes = np.array([[0] * len(np.unique(labels)) for i in range(len(data))], dtype='object')
-        nodes[:,0] = labels
+        nodes = np.array([[0] * len(np.unique(self.labels)) for i in range(len(self.data))], dtype='object')
+        nodes[:,0] = self.labels
     
-        for l in np.unique(labels):
+        for l in np.unique(self.labels):
             if(l != -1):
-                nodes[nodes[:,0] == str(l), class_map[l]] = 1
+                nodes[nodes[:,0] == str(l), self.storage['class_map'][l]] = 1
     
-        nodes[nodes[:,0] == -1,1:] = 1/c
+        nodes[nodes[:,0] == -1,1:] = 1/self.c
     
         return nodes
     
-    def __genDistTable(self, data, particles):
+    def __genDistTable(self):
     
-        dist_table = np.array([[len(data)-1] * len(particles) for i in range(len(data))])
+        dist_table = np.array([[len(self.data)-1] * len(self.storage['particles']) for i in range(len(self.data))])
     
-        for i in range(0,len(particles)):
-            dist_table[particles[i,1],i] = 0
+        for i in range(0,len(self.storage['particles'])):
+            dist_table[self.storage['particles'][i,1],i] = 0
     
         return dist_table
     
-    def __genGraph(self, data):
+    def __genGraph(self):
     
-        data_v = data.values
-        graph = {}
+        values = self.data.values
+        self.graph = {}
     
-        dist = np.array([[float("inf")] * len(data_v) for i in range(len(data_v))])
+        dist = np.array([[float("inf")] * len(values) for i in range(len(values))])
     
-        for i in range(0,len(data_v)):
+        for i in range(0,len(values)):
     
-            actual = data_v[i]
+            actual = values[i]
     
-            for j in range(i+1,len(data_v)):
-                    dist[j,i] = dist[i,j] = np.linalg.norm(actual-data_v[j])
+            for j in range(i+1,len(values)):
+                    dist[j,i] = dist[i,j] = np.linalg.norm(actual-values[j])
     
-        for i in range(0,len(data_v)):
+        for i in range(0,len(values)):
             sorted_dist = np.argsort(dist[i])
-            graph[i] = list(sorted_dist[0:self.n_neighbours])
+            self.graph[i] = list(sorted_dist[0:self.n_neighbors])
     
-        for i in range(0,len(data)):
-            graph[i] += ([k for k,v in graph.items() if i in v])
-            graph[i] = list(set(graph[i]))
+        for i in range(0,len(self.data)):
+            self.graph[i] += ([k for k,v in self.graph.items() if i in v])
+            self.graph[i] = list(set(self.graph[i]))
     
-        return graph
+        return self.graph
+    
